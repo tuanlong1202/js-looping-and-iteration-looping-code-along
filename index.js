c037a797a65fd719fc9127da4918f3d341c8d245
tuanlong1202 <tuanlong1202@gmail.com>
2021-04-04 20:19:03 -0400

    js looping and iteration looping

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arrName = [], strImpress) {
+    var name;
+    var arrThanks = [];
+    var strMessae = "";
+    for (let i = 0; i < arrName.length; i++) {
+        arrThanks[i] = `Thank you, ${arrName[i]}, for the wonderful ${strImpress} gift!`;
+    }
+    return arrThanks;
+}
+
+function countDown(intNumberProvide) {
+    for (let i = intNumberProvide; i >=0; i--){
+        console.log(i);
+    }
+}
